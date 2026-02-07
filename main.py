99b68456be273ae74a1504723ec6acac8b5f3a85
grommicus <tqwhite84@gmail.com>
2026-02-07 16:41:44 -0600

    locked to 60fps

--- main.py
+++ main.py
@@ -6,12 +6,16 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}\nScreen width: {SCREEN_WIDTH}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
+    clock = pygame.time.Clock()
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
         pygame.display.flip()
 if __name__ == "__main__":
     main()
